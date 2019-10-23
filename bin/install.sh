e9e8fabbd2f437513b8f35ff33ce6ceb46ea5ad3
jasenmichael <jasen@ready-builder.com>
2019-10-23 00:31:28 -0500

    edit install script

--- bin/install.sh
+++ bin/install.sh
@@ -14,14 +14,19 @@ else
     git clone git@github.com:jasenmichael/pm2-ui.git pm2-ui
     cd $INSTALL_DIR
 fi
-# todo: check and or install pm2
+
+pm2 delete pm2-ui >/dev/null 2>&1
+sleep 2
+
 echo "RUNNING: npm install"
 npm install >/dev/null 2>&1
-echo "RUNNING: npm run build"
-npm run build >/dev/null 2>&1
 sleep 2
+
+echo "RUNNING: PORT=8085 HOST=0.0.0.0 npm run build"
+PORT=8085 HOST=0.0.0.0 npm run build >/dev/null 2>&1
+sleep 2
+
 echo "RUNNING: PORT=8085 HOST=0.0.0.0 pm2 start -n pm2-ui npm -- start"
-pm2 delete pm2-ui >/dev/null 2>&1
 PORT=8085 HOST=0.0.0.0 pm2 start -n pm2-ui npm -- start >/dev/null 2>&1
 pm2 save >/dev/null 2>&1
 
